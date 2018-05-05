37117a9ece904ec1c979841c3603abe3f21318b9
felixlaimer <laimer.felix@hotmail.com>
2018-05-05 23:09:20 +0200

    MAX31856 boards share SDI/SDO/SCK port

--- TempLogSerial/TempLogSerial_MAX31856.ino
+++ TempLogSerial/TempLogSerial_MAX31856.ino
@@ -1,37 +1,37 @@
 /*****
-Sends 2 Adafruit Temp Values via Serial, programmed for two amplifier boards
+Sends 3 Adafruit Temp Values via Serial, programmed for three Adafruit MAX31856 amplifier boards
 *****/
 
 #include <SPI.h>
 #include <stdio.h>
 #include "Adafruit_MAX31856.h"
 
-int i = 0;
 String strng = "";
 char command;
 
 
 // digital IO pins.
-#define CS1   2
-#define DO1   3
-#define CLK1  4
-#define SDI1   8
-//#define CS2   5
-//#define DO2   6
-//#define CLK2  7
-//#define SDI2  9
+#define SCK  2
+#define SDO  3
+#define SDI  4
+#define CS1  8
+#define CS2  9
+#define CS3  10
 
 // initialize the Thermocouples
-Adafruit_MAX31856 thermocouple1 = Adafruit_MAX31856(CS1, SDI1, DO1, CLK1);
-//Adafruit_MAX31856 thermocouple2 = Adafruit_MAX31856(CS2, SDI2, DO2, CLK2);
+Adafruit_MAX31856 thermocouple1 = Adafruit_MAX31856(CS1, SDI, SDO, SCK);
+Adafruit_MAX31856 thermocouple2 = Adafruit_MAX31856(CS2, SDI, SDO, SCK);
+Adafruit_MAX31856 thermocouple3 = Adafruit_MAX31856(CS3, SDI, SDO, SCK);
 
 void setup() {  
   // initialize serial:
   Serial.begin(9600);
   thermocouple1.begin();
-  //thermocouple2.begin();
+  thermocouple2.begin();
+  thermocouple3.begin();
   thermocouple1.setThermocoupleType(MAX31856_TCTYPE_K);
-  //thermocouple2.setThermocoupleType(MAX31856_TCTYPE_K);
+  thermocouple2.setThermocoupleType(MAX31856_TCTYPE_K);
+  thermocouple3.setThermocoupleType(MAX31856_TCTYPE_K);
 
   establishContact();  // send a byte to establish contact until receiver responds
 }
@@ -67,11 +67,18 @@ void loop() {
       // print temperatures on *TMP? command
       if (strng.equals("*TMP?")){
         // send temperatures
-        double T1 = thermocouple1.readThermocoupleTemperature();
-        //double T2 = thermocouple2.readThermocoupleTemperature();
+       double T1 = thermocouple1.readThermocoupleTemperature();
+       double T2 = thermocouple2.readThermocoupleTemperature();
+       double T3 = thermocouple3.readThermocoupleTemperature();
+
+       // internal temperature reading
+       // double T1 = thermocouple1.readCJTemperature();
+       // double T2 = thermocouple2.readCJTemperature();
+       // double T3 = thermocouple3.readCJTemperature();
 
         String sT = String(T1,2) + ",";
-        //sT += String(T2,2) + ",";
+        sT += String(T2,2) + ",";
+        sT += String(T3,2) + ",";
         Serial.println(sT);
         strng = "";
       } 
